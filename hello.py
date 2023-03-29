e666d67791e7bf6d51b2e65d220982a4c1eb68d8
Angeth-Herjok <angethherjok@gmail.com>
2023-03-29 19:19:30 +0300

    first commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+def hello(hello):
+    print(f"Hello{name}")
+    hello(Rebecca)
+
+    def welcome(student,school):
+        print(f"welcome to {school}, {student}")
---
+++ math.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    answer=a+b
+    return answer
+
+def substract(c,d):
+    difference=c-d
+    return difference
+
+def divide(e,f):
+    dividend=e/f
+    return dividend
+
+def multiply(m,n):
+    product=m*n
+    return product
+
+def remainder(y,z):
+    modulus=y%z
+    return modulus
